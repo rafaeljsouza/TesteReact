20c5986f0c355247d4f0b89c859fd3d18a73b1f5
Jorge Riboldi <jfriboldi@gmail.com>
2024-12-02 15:05:13 -0300

    add input

--- app/__tests__/products.test.js
+++ app/__tests__/products.test.js
@@ -6,7 +6,19 @@ describe("Products", () => {
   it("mostra um input", () => {
     render(<Products />);
 
-    const input = screen.getByPlaceholderText("Digite o nome do produto");
+    const input = screen.getByPlaceholderText("Pesquisar produtos");
+
+    expect(input).toBeInTheDocument();
+  });
+});
+
+describe("Products", () => {
+  it("mostra todos os produtos caso não há nenhuma pesquisa", () => {
+    render(<Products />);
+
+    const results = screen.getAllByTestId("product");
+
+    expect(results.length).toBeGreaterThan(0);
 
     expect(input).toBeInTheDocument();
   });
--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -1,18 +1,33 @@
-'use client'
+import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
 
 
 export default function Products() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <input
-        type="text"
-        placeholder="Digite o nome do produto"
-        className="border p-2"
-        id="search"
-      />
-      <div className="filtered-results">
+    <div className="w-full flex justify-center flex-col h-full">
 
+      <div className="border-gray-500 w-1/2 mx-auto mb-4">
+        <label htmlFor="search" className="block text-sm/6 font-medium text-gray-900">
+          Pesquisa
+        </label>
+        <div className="mt-2 grid grid-cols-1">
+          <input
+            id="search"
+            name="search"
+            type="search"
+            placeholder="Pesquisar produtos"
+            className="col-start-1 row-start-1 block w-full rounded-md bg-white py-1.5 pl-10 pr-3 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:pl-9 sm:text-sm/6"
+          />
+          <MagnifyingGlassIcon
+            aria-hidden="true"
+            className="pointer-events-none col-start-1 row-start-1 ml-3 size-5 self-center text-gray-400 sm:size-4"
+          />
+        </div>
+      </div>
+
+      <div className="mb-4 border-b border-1"></div>
+      <div>
+        Produtos aqui
       </div>
     </div>
   )
